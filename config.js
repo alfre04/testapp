ba56a6d68acfa890a61edc8c792f0064582fbf5d
Alfredo Govea <agovea@mexltagovea-3.local>
2018-01-29 18:28:54 -0600

    Test

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'android',
+		wp_ws_url : 'http://warroom2018.com/wp-appkit-api/android',
+		wp_url : 'http://warroom2018.com',
+		theme : 'q-android',
+		version : '1.0',
+		app_title : 'WR2018',
+		app_platform : 'android',
+		gmt_offset : -6,
+		debug_mode : 'off',
+		auth_key : 'put your unique phrase here',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});

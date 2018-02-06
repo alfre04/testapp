ae0f1b15b6202653cb0d5773f9b3cfa09613bf26
Alfredo Govea <agovea@mexltagovea-3.local>
2018-02-06 10:54:09 -0600

    js

--- themes/wpak-theme-bootstrap-feature-user-login/js/functions.js
+++ themes/wpak-theme-bootstrap-feature-user-login/js/functions.js
@@ -1307,25 +1307,6 @@ define( [ 'jquery',
 	   return template_args;
 	} );
 	
-	var analytics = navigator.analytics;
-
-	// set the tracking id
-	analytics.setTrackingId('UA-113623932-1');
-	
-	analytics.sendAppView('home', successCallback, errorCallback);
-	
-	// or the same could be done using the send function
-	
-	var Fields    = analytics.Fields,
-		HitTypes  = analytics.HitTypes,
-		LogLevel  = analytics.LogLevel,
-		params    = {};
-	
-	params[Fields.HIT_TYPE]     = HitTypes.APP_VIEW;
-	params[Fields.SCREEN_NAME]  = 'home';
-	
-	analytics.setLogLevel(LogLevel.INFO);
-	
-	analytics.send(params, successCallback, errorCallback);
+	window.ga.startTrackerWithId('UA-113623932-1', 30);
 
 } );
